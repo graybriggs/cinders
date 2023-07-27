7173f487b5ad82b062669352b47d8c070fcf5f3b
Graham Briggs <grahambriggs@gmail.com>
2023-07-27 21:14:08 +0900

    cinders particle demo

---
+++ cinders.cpp
@@ -0,0 +1,139 @@
+
+#include <cmath>
+#include <numbers>
+#include <SDL.h>
+
+#include "cinders.h"
+
+int ORIGIN_X = 600;
+int ORIGIN_Y = 800;
+
+bool gravity = true;
+int gravity_value = 1;
+
+float angle = 90.0f;
+
+constexpr float PI = 3.14159265;
+
+
+
+void init_cinder(Cinder& cinder) {
+	
+	cinder.x = ORIGIN_X - 5 + rand() % 16;
+	cinder.y = ORIGIN_Y - rand() % 4;
+
+	cinder.vel_x = (-2 + rand() % 5) * 1.0f;
+	cinder.vel_y = (-10 + rand() % 5) * 1.0f;
+
+	//cinders[i].color = SDL_Color(255, 0, 0);
+
+	cinder.lifetime = 20 + rand() % 10000;
+	//cinders[i].lifetime = 60;
+
+	cinder.color_counter = 0;
+
+	cinder.gravity_counter = 0;
+
+	// how long it will take cinder to cool
+	cinder.color_threshold = 4 + rand() % 8;
+	// how long it will take for gravity to take effect.
+	cinder.gravity_threshhold = 1 + rand() % 1500;
+
+}
+
+void init_all_cinders(Cinders& cinders) {
+
+	for (int i = 0; i < cinders.size(); ++i) {
+		init_cinder(cinders[i]);
+	}
+}
+
+void reset_cinder(Cinder& cinder) {
+
+	init_cinder(cinder);
+}
+
+void rotate_left(Cinders& cinders) {
+
+	for (int i = 0; i < cinders.size(); ++i) {
+		cinders[i].vel_x--;
+		cinders[i].vel_y--;
+	}
+
+
+	angle -= 2.0f;
+	if (angle < 0.0f)
+		angle = 360.0f;
+}
+
+void rotate_right() {
+	angle += 2.0f;
+	if (angle > 360.0f)
+		angle = 0.0f;
+}
+
+void move_x(float amount) {
+	ORIGIN_X += amount;
+}
+void move_y(float amount) {
+	ORIGIN_Y += amount;
+}
+
+void toggle_gravity() {
+	gravity = !gravity;
+}
+
+void move_cinders(Cinders& cinders, double dt) {
+
+	for (int i = 0; i < cinders.size(); ++i) {
+
+		cinders[i].x += cinders[i].vel_x * dt;
+		cinders[i].y += cinders[i].vel_y * dt;
+
+		// apply gravity
+
+		if (gravity) {
+			cinders[i].gravity_counter++;
+
+			if (cinders[i].gravity_counter >= cinders[i].gravity_threshhold) {
+				cinders[i].vel_y++;
+
+				cinders[i].gravity_counter = 0;
+			}
+		}
+
+		cinders[i].lifetime--;
+
+		if (cinders[i].lifetime <= 0) {
+			reset_cinder(cinders[i]);
+		}
+		
+		else if (cinders[i].x <= 0 || cinders[i].x > 1200) {
+			reset_cinder(cinders[i]);
+		}
+		
+		else if (cinders[i].y > 800) {
+			reset_cinder(cinders[i]);
+		}
+
+	}
+}
+
+void draw_cinders(SDL_Renderer* renderer, Cinders& cinders) {
+
+	for (int i = 0; i < cinders.size(); ++i) {
+		if (cinders[i].y >= 0 && cinders[i].y < 800) {
+			SDL_Rect r;
+			r.x = cinders[i].x;
+			r.y = cinders[i].y;
+			r.w = 2;
+			r.h = 2;
+			SDL_RenderFillRect(renderer, &r);
+		}
+	}
+}
+
+float angle_to_radians(float angle) {
+	// C++20 has std::numbers::pi
+	return angle * PI / 180;
+}
---
+++ cinders.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <cstdlib>
+#include <vector>
+
+#include <SDL.h>
+
+struct Cinder {
+	float x;
+	float y;
+	float vel_x;
+	float vel_y;
+	int lifetime;
+	SDL_Color color;
+	// tracks when color will change
+	int color_counter;  
+	int color_threshold;
+	// tracks when gravity will be applied
+	int gravity_counter; 
+	int gravity_threshhold;
+
+	int current_color;
+};
+
+constexpr int MAX_CINDERS = 512;
+
+
+typedef std::array<Cinder, MAX_CINDERS> Cinders;
+//Cinders cinders;
+
+
+void init_cinder(Cinder& cinder);
+void init_all_cinders(Cinders& cinders);
+
+void move_x(float amount);
+void move_y(float amount);
+void rotate_left(Cinders& cinders);
+void rotate_right();
+void toggle_gravity();
+//void increase_gravity();
+//void decrease_gravity();
+
+void move_cinders(Cinders& cinders, double dt);
+void draw_cinders(SDL_Renderer* renderer, Cinders& cinders);
+
+
+float angle_to_radians(float angle);
